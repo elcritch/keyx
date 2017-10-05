73a739213fc315d89be0df0a16253239912f12bf
Jaremy Creechley <creechley@gmail.com>
2017-10-05 17:54:16 -0600

    Adding example code

--- native/rustysecretsnif/src/lib.rs
+++ native/rustysecretsnif/src/lib.rs
@@ -3,6 +3,8 @@
 #[macro_use] extern crate lazy_static;
 
 use rustler::{NifEnv, NifTerm, NifResult, NifEncoder};
+use std::sync::RwLock;
+use rustler::resource::ResourceArc;
 
 mod atoms {
     rustler_atoms! {
@@ -15,13 +17,69 @@ mod atoms {
 
 rustler_export_nifs! {
     "Elixir.RustySecretsNif",
-    [("add", 2, add)],
-    None
+    // The functions we want to export.
+	// They consist of a tuple containing the function name,
+	// function arity, and the Rust function itself.
+	[
+		("new", 1, buffer_new),
+		("get", 2, buffer_get),
+		("set", 3, buffer_set),
+	],
+	// Our on_load function. Will get called on load.
+	Some(on_init)
 }
 
-fn add<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
-    let num1: i64 = try!(args[0].decode());
-    let num2: i64 = try!(args[1].decode());
+// The `'a` in this function definition is something called a lifetime.
+// This will inform the Rust compiler of how long different things are
+// allowed to live. Don't worry too much about this, as this will be the
+// exact same for most function definitions.
+fn on_init<'a>(env: NifEnv<'a>, _load_info: NifTerm<'a>) -> bool {
+	// This macro will take care of defining and initializing a new resource
+	// object type.
+	resource_struct_init!(Buffer, env);
+	true
+}
 
-    Ok((atoms::ok(), num1 + num2).encode(env))
+struct Buffer {
+	data: RwLock<Vec<u8>>,
+}
+
+
+fn buffer_new<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+	// The NIF should have a single argument provided, namely
+	// the size of the buffer we want to create.
+	let buffer_size: usize = args[0].decode()?;
+
+	// Create the actual buffer and initialize it with zeroes.
+	let mut buffer = Vec::with_capacity(buffer_size);
+	for _i in 0..buffer_size {
+        buffer.push(0);
+    }
+
+	// Make the actual struct
+	let buffer_struct = Buffer {
+		data: RwLock::new(buffer),
+	};
+
+	// Return it!
+	Ok(ResourceArc::new(buffer_struct).encode(env))
+}
+
+fn buffer_get<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+	let buffer: ResourceArc<Buffer> = args[0].decode()?;
+	let offset: usize = args[1].decode()?;
+
+    let buf = buffer.data.read().unwrap();
+
+	let byte = buf[offset];
+	Ok(byte.encode(env))
+}
+
+fn buffer_set<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+	let buffer: ResourceArc<Buffer> = args[0].decode()?;
+	let offset: usize = args[1].decode()?;
+	let byte = args[2].decode()?;
+
+	buffer.data.write().unwrap()[offset] = byte;
+	Ok(byte.encode(env))
 }
---
+++ native/rustysecretsnif/target/debug/deps/rustysecretsnif.d
@@ -0,0 +1,5 @@
+/Users/elcritch/proj/finix/dev/keyx/native/rustysecretsnif/target/debug/deps/librustysecretsnif.dylib: src/lib.rs
+
+/Users/elcritch/proj/finix/dev/keyx/native/rustysecretsnif/target/debug/deps/rustysecretsnif.d: src/lib.rs
+
+src/lib.rs:
