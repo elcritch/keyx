98c8c935b01d62a29626d27b79469349dab9b12a
Jaremy Creechley <creechley@gmail.com>
2017-10-07 17:46:26 -0600

    Renaming to KeyX for readibility.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KeyX.Mixfile do
   def project do
     [
       app: :keyx,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.3",
       start_permanent: Mix.env == :prod,
       deps: deps(),
