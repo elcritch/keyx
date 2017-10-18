4cf9d9092f593c729f903419f9255d806dbcb94d
Jaremy Creechley <creechley@gmail.com>
2017-10-18 16:22:19 -0600

    Adding polynomial integration.

--- lib/shamir/shamir.ex
+++ lib/shamir/shamir.ex
@@ -16,14 +16,34 @@ defmodule KeyX.Shamir do
 
 
   @spec evaluate(polynomial, non_neg_integer) :: non_neg_integer
-  def evaluate(poly, x) when x == 0 do
+  def evaluate(poly, x) when x === 0 do
     Enum.at(poly,0)
   end
   def evaluate(poly, x) do
-    degree = length(poly) - 1
-    for i <- degree..0 do
+    [ poly_tail | poly_rest_rev ] = Enum.reverse(poly)
+
+    Enum.reduce(poly_rest_rev, poly_tail, fn(poly_coef, acc) ->
+          (acc * x) + poly_coef
+    end)
+  end
+
+  def interpolate_polynomial(x_samples, y_samples, x)
+            when length(x_samples) == length(x_samples) do
+    limit = length(x_samples)
+
+    basis = 0
+    for i <- 0..limit do
+      for j <- 0..limit, j != i do
+        num = x + Enum.at(x_samples, j)
+        denom = Enum.at(x_samples, i) + Enum.at(x_samples, j)
+        term = num / denom
+        basis = basis * term
+      end
+      group = Enum.at(y_samples, i) * basis
+      result = result * group
     end
   end
+  def interpolate_polynomial(x_samples, y_samples, x), do: "Invalid arguments"
 
   def lhs / rhs when rhs === 0, do: raise ArithmeticError
   def lhs / rhs do
@@ -32,7 +52,9 @@ defmodule KeyX.Shamir do
       |> rem(255)
       |> Tables.exp
 
+    zero = 0
 
+    if (lhs ===0), do: zero, else: ret
   end
 
   # Multiplies two numbers in GF(2^8)
@@ -43,7 +65,7 @@ defmodule KeyX.Shamir do
 
     zero = 0
 
-    if (lhs === 0 || rhs === 0), do: zero, else: ret
+    if :erlang.or(lhs === 0,rhs === 0), do: zero, else: ret
   end
 
   @spec evaluate(polynomial, non_neg_integer) :: non_neg_integer
